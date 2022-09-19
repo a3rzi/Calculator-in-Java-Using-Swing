2a29f541c2d171fa29bf6bcb9bc90a77b777ad8e
a3rzi <amirhosseinqatar@gmail.com>
2022-09-19 12:04:43 +0430

    dadeha vared shodan

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class CarTest {
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    @Test
+    void CARNAME() {
+        Mashine samand = new Mashine("a");
+        System.out.println(samand.name);
+        Mashine pride = new Mashine("pride");
+        System.out.println(pride.name);
+
+
+
+    }
+    class Mashine{
+        int speed;
+        int model;
+        String name;
+
+         Mashine(){
+            System.out.println("mashine ezafe shod");
+        }
+        Mashine(String mark){
+            this.name=mark;
+        }
+        void drive(){
+            System.out.println("drive in the road");
+        }
+        void tormoz(){
+            System.out.println("tormoz kon");
+        }
+
+    }
 }
+
+
